3c3bf11f50f865b265bd3350d81fd0b868fc7a8b
Combatd <combatdstar@gmail.com>
2021-03-20 16:00:00 -0700

    Declare variables with complex types

--- first-steps/src/App.tsx
+++ first-steps/src/App.tsx
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
-    // let firstValue: string = "Manny";
-    // let firstValue: number = 34;
-    // let firstValue: boolean = true;
-    // let firstValue: number[] = [2, 3, 56];
-    let firstValue: Array<string> = ['2', '3', 'Manny'];
+    // tuple
+    let aTuple: [string, number] = ['Manny', 34];
+    // enum
+    enum Codes {first = 1, second = 2};
+    // any
+    let firstName: any = "Manny"
+    // void
+    const warning = (): void => {
+      console.log('warning');
+    }
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            The value {firstValue} is of {typeof firstValue} type!
+            The value {firstName} is of {typeof firstName} type!
           </p>
         </header>
       </div>
