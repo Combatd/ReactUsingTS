183b109378a7802bcf4060fd29200483d375a850
Combatd <combatdstar@gmail.com>
2021-03-20 15:37:01 -0700

    Declare explicitly typed variables

--- first-steps/src/App.tsx
+++ first-steps/src/App.tsx
@@ -4,21 +4,19 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // let firstValue: string = "Manny";
+    // let firstValue: number = 34;
+    // let firstValue: boolean = true;
+    // let firstValue: number[] = [2, 3, 56];
+    let firstValue: Array<string> = ['2', '3', 'Manny'];
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            The value {firstValue} is of {typeof firstValue} type!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
